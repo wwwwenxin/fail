a407d22bfbd142ae552553e391fcc41c70a828e0
wwwwenxin <578355367@qq.com>
2015-11-11 21:36:34 +0800

    第一次提交
    简易指针

--- fail/main.cpp
+++ fail/main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  fail
+//  zhizhen
 //
 //  Created by wx on 15/11/11.
 //  Copyright © 2015年 wx. All rights reserved.
@@ -10,6 +10,11 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int *a;
+    a=(int *)malloc(4);
+    if(a==0)
+        exit(0);
+    *a=100;
+    printf("a=%x *a=%x\n",a,*a);
     return 0;
 }
