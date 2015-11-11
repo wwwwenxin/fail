8cbbbf39b374f2caf0f0118ee55627c257e848d7
wwwwenxin <578355367@qq.com>
2015-11-11 21:35:28 +0800

    Initial Commit

---
+++ fail/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  fail
+//
+//  Created by wx on 15/11/11.
+//  Copyright © 2015年 wx. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
